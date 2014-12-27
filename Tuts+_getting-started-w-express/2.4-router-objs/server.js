84251a800b9f201a8c6210469e8e27fcf2af84ee
Apollo Tang <tang.apollo@gmail.com>
2014-12-27 09:51:24 -0500

    tuts+ gettting-started: v2.4--3:24 | express.Router(), use it to prepend a url path, added comment

--- Tuts+_getting-started-w-express/2.4-router-objs/server.js
+++ Tuts+_getting-started-w-express/2.4-router-objs/server.js
@@ -9,77 +9,27 @@ var express = require('express'),
 //      3. verb/all
 //      4. route
 
-var router = express.Router();
+var myRouter = express.Router();
 
 
-router.use(function(req, res, next) {
+myRouter.use(function(req, res, next) {
     console.log('router specific middleware');
     next();
 });
 
-router.get('/', function(req, res){
+myRouter.get('/', function(req, res){
     res.send('router home route');
 });
 
-router.route('/test').get(function(req, res){
+myRouter.route('/test').get(function(req, res){
     res.send('router test route');
 });
 
-app.use('/api', router);
+app.use('/api', myRouter);
 
 // localhost:3000/api/
 // localhost:3000/api/test
+                  //^^^^------------  Note that express.Router()
+                  //                       let you prepend your url path
 
 
-/*
- * // 1. "use" is for middleware
- *
- *     //** custom middleware
- *         app.use(function (req, res, next){
- *             console.log("this will log on every request");
- *             next();
- *         });
- *
- * // 2. "param" is a middleware to parse parameter
- *
- *     app.param('myParameter', function( req, res, next , yourParameter ){
- *               // ^-----  the first argument for app.param() map to parameter you want
- *               //         to match in the route bellow
- *               //         and its value is availble as the 4th argument in callback (as "yourParameter")
- *
- *         req.someVar = yourParameter;
- *         req.someVar2 = yourParameter[0].toUpperCase()  // Capitalize first character
- *                      + yourParameter.substring(1);     // Concated all charecters after first charecter
- *         next();
- *     });
- *
- * // 3. "verb" method
- *
- *     app.all('/', function(req, res, next){
- *         // will run for all CRUD verb
- *         console.log('from ALL');
- *         next(); // without this app.get() and app.post() after will not be executed
- *     });
- *
- *     app.get('/name/:myParameter', function(req,res){
- *         res.send( 'req.someVar2 = ' + req.someVar2 + '</br>');
- *     });
- *
- * // 4. route
- *
- *      app.route('/using-route/')
- *         .all(
- *             function(req, res, next){
- *                 // will run for all CRUD verb
- *                 console.log('from ALL');
- *                 next(); // without this app.get() and app.post() after will not be executed
- *             }
- *         )
- *         .get(
- *             function(req, res){
- *                 res.send('using route');
- *             }
- *         );
- */
-
-    app.listen(3000);
